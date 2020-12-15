2fbfe1fa7ff1c5ba8470ccbdc6df53d69fa292bf
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-15 14:30:30 +0200

    initialised store for user

---
+++ src/app/models/backend/user/index.ts
@@ -0,0 +1,10 @@
+export interface User {
+  uid: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+  cellNumber: string;
+  photoUrl: string;
+  created: firebase.default.firestore.FieldValue;
+  updated?: firebase.default.firestore.FieldValue;
+}
