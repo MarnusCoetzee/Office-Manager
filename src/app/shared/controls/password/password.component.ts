45e512dc8148c002cc1ef2f946670a61fdf399c0
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-15 13:01:51 +0200

    finished password control

--- src/app/shared/controls/password/password.component.ts
+++ src/app/shared/controls/password/password.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  forwardRef,
+  Input,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+
+type PasswordType = 'password' | 'text';
 
 @Component({
   selector: 'app-password',
   templateUrl: './password.component.html',
-  styleUrls: ['./password.component.scss']
+  styleUrls: ['./password.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => PasswordComponent),
+      multi: true,
+    },
+  ],
 })
-export class PasswordComponent implements OnInit {
+export class PasswordComponent implements OnInit, ControlValueAccessor {
+  @Input() placeholder: string;
 
-  constructor() { }
+  @Output() changed = new EventEmitter<string>();
 
-  ngOnInit(): void {
+  value: string;
+  isDisabled: boolean;
+  passwordType: PasswordType;
+
+  constructor() {
+    this.passwordType = 'password';
   }
 
+  ngOnInit(): void {}
+
+  private propagateChange: any = () => {};
+  private propagateTouched: any = () => {};
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propagateTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  onKeyup(value: string): void {
+    this.value = value;
+    this.propagateChange(value);
+    this.changed.emit(value);
+  }
+
+  onBlur(): void {
+    this.propagateTouched();
+  }
+
+  togglePassword(): void {
+    this.passwordType = this.passwordType === 'password' ? 'text' : 'password';
+  }
 }
