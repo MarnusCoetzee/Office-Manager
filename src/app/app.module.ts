817e23c8c03b253b248cb999880e8ba5e99ceb78
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-15 12:06:37 +0200

    init home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,13 @@ import { AppComponent } from './app.component';
 import { environment } from '../environments/environment.prod';
 
 // Store
+// @ts-ignore
 // import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-const StoreDevtools = !environment.production
-  ? StoreDevtoolsModule.instrument({ maxAge: 50 })
-  : [];
+// import { EffectsModule } from '@ngrx/effects';
+// import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+// const StoreDevtools = !environment.production
+//   ? StoreDevtoolsModule.instrument({ maxAge: 50 })
+//   : [];
 
 // Date
 import {
@@ -39,6 +40,7 @@ import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -51,7 +53,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AngularFireAuthModule,
     AngularFireStorageModule,
     BrowserAnimationsModule,
-    StoreDevtools,
+    SharedModule,
+    // StoreDevtools,
   ],
   providers: [
     { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
