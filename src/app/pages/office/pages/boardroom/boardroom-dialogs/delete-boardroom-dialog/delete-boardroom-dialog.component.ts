86cf31d1271faaf04ff55e1383debdc47248f074
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-23 21:01:50 +0200

    finished delete dialog + store

--- src/app/pages/office/pages/boardroom/boardroom-dialogs/delete-boardroom-dialog/delete-boardroom-dialog.component.ts
+++ src/app/pages/office/pages/boardroom/boardroom-dialogs/delete-boardroom-dialog/delete-boardroom-dialog.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BoardRoom } from '@app/models/backend';
+import { Store } from '@ngrx/store';
+import * as fromRoot from '@app/store';
+import * as fromList from '../../store/boardrooms';
 @Component({
   selector: 'app-delete-boardroom-dialog',
   templateUrl: './delete-boardroom-dialog.component.html',
-  styleUrls: ['./delete-boardroom-dialog.component.scss']
+  styleUrls: ['./delete-boardroom-dialog.component.scss'],
 })
 export class DeleteBoardroomDialogComponent implements OnInit {
+  boardroom: BoardRoom;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private dialogRef: MatDialogRef<DeleteBoardroomDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data,
+    private store: Store<fromRoot.State>
+  ) {
+    this.boardroom = data.boardroom;
   }
 
+  ngOnInit(): void {
+    console.log(this.boardroom);
+  }
+
+  onClickDeleteBoardroom() {
+    this.store.dispatch(new fromList.Delete(this.boardroom));
+    this.dialogRef.close();
+  }
+
+  onClickCloseDialog() {
+    this.dialogRef.close();
+  }
 }
