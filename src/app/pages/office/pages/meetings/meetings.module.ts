9d2e8f2a8d743d7a8f46b021869d3668f4162f54
MarnusCoetzee <marnuscoe@gmail.com>
2020-12-27 12:37:54 +0200

    initialised meetings component and module

---
+++ src/app/pages/office/pages/meetings/meetings.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeetingsRoutingModule } from './meetings-routing.module';
+import { MeetingsComponent } from './meetings.component';
+
+
+@NgModule({
+  declarations: [MeetingsComponent],
+  imports: [
+    CommonModule,
+    MeetingsRoutingModule
+  ]
+})
+export class MeetingsModule { }
