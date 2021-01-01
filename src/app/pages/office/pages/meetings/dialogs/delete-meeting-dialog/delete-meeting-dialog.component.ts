d8e2a10c696d972e1fd463a0cec19da3590cd919
MarnusCoetzee <marnuscoe@gmail.com>
2021-01-01 16:45:14 +0200

    finished delete meeting dialog

--- src/app/pages/office/pages/meetings/dialogs/delete-meeting-dialog/delete-meeting-dialog.component.ts
+++ src/app/pages/office/pages/meetings/dialogs/delete-meeting-dialog/delete-meeting-dialog.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Meeting } from '@app/models/backend';
+import * as fromRoot from '@app/store';
+import { Store } from '@ngrx/store';
+import * as fromMeetings from '../../store/meetings-store';
 @Component({
   selector: 'app-delete-meeting-dialog',
   templateUrl: './delete-meeting-dialog.component.html',
-  styleUrls: ['./delete-meeting-dialog.component.scss']
+  styleUrls: ['./delete-meeting-dialog.component.scss'],
 })
 export class DeleteMeetingDialogComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  meeting: Meeting;
+  constructor(
+    private dialogRef: MatDialogRef<DeleteMeetingDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data,
+    private store: Store<fromRoot.State>
+  ) {
+    this.meeting = data.meeting;
   }
 
+  ngOnInit(): void {}
+
+  onClickCloseDialog() {
+    this.dialogRef.close();
+  }
+
+  onClickDeleteMeeting() {
+    this.store.dispatch(new fromMeetings.Delete(this.meeting));
+    this.dialogRef.close();
+  }
 }
